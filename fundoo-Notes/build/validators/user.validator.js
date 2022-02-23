11bbb11450002ae176dc166f6ec3882239b1d2af
saurabhyeggewar1998 <yeggewar@gmail.com>
2022-02-23 10:26:35 +0530

    Registration

---
+++ fundoo-Notes/build/validators/user.validator.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.newUserValidator = void 0;
+
+var _joi = _interopRequireDefault(require("@hapi/joi"));
+
+var newUserValidator = function newUserValidator(req, res, next) {
+  var schema = _joi["default"].object({
+    firstName: _joi["default"].string().min(4).required(),
+    lastName: _joi["default"].string().min(4).required(),
+    email: _joi["default"].string().email().required(),
+    password: _joi["default"].string().required()
+  });
+
+  var _schema$validate = schema.validate(req.body),
+      error = _schema$validate.error,
+      value = _schema$validate.value;
+
+  if (error) {
+    next(error);
+  } else {
+    req.validatedBody = value;
+    next();
+  }
+};
+
+exports.newUserValidator = newUserValidator;
---
+++ fundoo-Notes/src/validators/user.validator.js
@@ -0,0 +1,17 @@
+import Joi from '@hapi/joi';
+
+export const newUserValidator = (req, res, next) => {
+  const schema = Joi.object({
+    firstName: Joi.string().min(4).required(),
+    lastName: Joi.string().min(4).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    next(error);
+  } else {
+    req.validatedBody = value;
+    next();
+  }
+};
