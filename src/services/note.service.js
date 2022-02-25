920c4e12d872ef0bd70ef7c6e53d9ded44843e4e
saurabhyeggewar1998 <95201793+saurabhyeggewar1998@users.noreply.github.com>
2022-02-25 21:46:46 +0530

    Added file Authorization

---
+++ src/services/note.service.js
@@ -0,0 +1,39 @@
+import Note from '../models/note.model';
+
+//create new note
+export const create = async (body) => {
+        const Notebody = await Note.create(body);
+          return Notebody;
+};
+
+//get all notes
+export const getAllNotes = async () => {
+    const data = await Note.find();
+    return data;
+  };
+
+//get single note
+export const getSingleNote = async (id) => {
+    const data = await Note.findById(id);
+    return data;
+  };
+  
+//update note
+export const updateNote = async (_id, body) => {
+    const data = await Note.findByIdAndUpdate(
+      {
+        _id
+      },
+      body,
+      {
+        new: true
+      }
+    );
+    return data;
+  };
+
+//delete note
+export const deleteNote = async (id) => {
+    await User.findByIdAndDelete(id);
+    return '';
+  };
