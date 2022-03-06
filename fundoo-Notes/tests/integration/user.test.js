cd82a6215dc36b7eeaf41177d5d416d42ed24ff6
saurabhyeggewar1998 <95201793+saurabhyeggewar1998@users.noreply.github.com>
2022-03-06 15:00:13 +0530

    [Saurabh Yeggewar] Added test for Api

--- fundoo-Notes/tests/integration/user.test.js
+++ fundoo-Notes/tests/integration/user.test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import request from 'supertest';
 import mongoose from 'mongoose';
+import HttpStatus from 'http-status-codes';
 
 import app from '../../src/index';
 
@@ -26,16 +27,90 @@ describe('User APIs Test', () => {
     done();
   });
 
-  describe('GET /users', () => {
-    it('should return empty array', (done) => {
-      request(app)
-        .get('/api/v1/users')
-        .end((err, res) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.body.data).to.be.an('array');
+  
+          describe(`POST /User`, () => {
+            it('should  register ', (done) => {
+               const user1 = {
+                firstName : "kumaraaa",
+                lastName : "kurmi",
+                email : "sachin@gmail.com",
+                password : "1234567"
+              };
 
-          done();
+              request(app)
+                .post('/api/v1/users/userregister')
+                .send(user1)
+                .end((err, res) => {
+                  expect(res.statusCode).to.be.equal(HttpStatus.CREATED);
+                  done();
+                });
+            });
+        });
+
+        describe(`login`, () => {
+
+          it('should login', (done) => {
+
+            const user1 = {
+              
+              email : "sachin@gmail.com",
+              password : "1234567"
+            };
+
+            request(app)
+              .post('/api/v1/users/login')
+              .send(user1)
+              .end((err, res) => {
+                expect(res.statusCode).to.be.equal(HttpStatus.OK);
+                done();
+              });
+          });
+      });
+
+
+      describe(`forgetPassword`, () => {
+
+        it('should forget', (done) => {
+
+          const user1 = {
+            
+            email : "sachin@gmail.com"
+           
+          };
+
+          request(app)
+            .post('/api/v1/users/forgetPassword')
+            .send(user1)
+            .end((err, res) => {
+              expect(res.statusCode).to.be.equal(HttpStatus.OK);
+              done();
+            });
         });
     });
+    describe(`resetPassword`, () => {
+
+      it('should reset', (done) => {
+
+        const user1 = {
+          
+          password:"rabhu963257"
+         
+        };
+        const newJwtoken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhY2hpbkBnbWFpbC5jb20iLCJpZCI6IjYyMjM4Y2I2NzBjN2M0NmJlNDQ0NDRhMCIsImlhdCI6MTY0NjQ5Njk1MX0.eHNoN8Tkv2odQ8HcsqtURFhbOeIJ0IvR6-y4iSDUo0Q ";
+        request(app)
+          .put('/api/v1/users/resetPassword')
+          .set('Authorization',`${newJwtoken}`)
+          .send(user1)
+          .end((err, res) => {
+            expect(res.statusCode).to.be.equal(HttpStatus.OK);
+            done();
+          });
+      });
   });
+
+
+    
+
 });
+
+
